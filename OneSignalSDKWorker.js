e63441c865fbb0b6818b1e28087a0a9d8f412671
Rafael Farias <farifran@gmail.com>
2025-10-26 15:41:52 -0300

    feat: Initialize OneSignal service worker
    Integrates the OneSignal SDK service worker by importing the script from the CDN. This enables push notification functionality for the application.

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,9 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Este script do service worker é essencial para o funcionamento das notificações push do OneSignal.
+// Ele simplesmente importa o script principal do SDK do OneSignal a partir de sua CDN.
+// Não adicione nenhum outro código a este arquivo, pois ele é gerenciado pelo OneSignal.
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
