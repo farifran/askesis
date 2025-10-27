9ad1bb1bd3aaa4887bd62c2c4e10cc5474261f65
Rafael Farias <farifran@gmail.com>
2025-10-27 01:04:04 -0300

    feat: Simplify OneSignal integration and service worker
    Removes redundant OneSignal initialization code and simplifies the service worker registration by directly importing the OneSignal SDK worker. This streamlines the PWA setup and ensures a cleaner integration with OneSignal's notification service.

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,5 +1,1 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
